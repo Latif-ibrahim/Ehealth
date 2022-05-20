c6d0af118dbfe1cca7ca981135a33af191691e5a
Latif-Ibrahim <ibrahimabdulatif1@gmail.com>
2022-05-20 02:52:17 -0700

    added data

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,5 @@
 import { Link } from 'react-router-dom'
-import React, { useContext, useState, useEffect } from "react";
-import { AuthContext, AuthContextProvider } from "../context/auth.context";
-import { AdminContext } from "../context/user.context";
-import { FaSignOutAlt, FaUser } from "react-icons/fa";
-import { useNavigate } from "react-router-dom";
+import React from "react";
 import { VscAccount } from "react-icons/vsc";
 
 
@@ -59,53 +55,7 @@ function NavBar() {
       </ul>
       
     </nav>
-  );
-  
-  const navigate = useNavigate();
-    const { success, loading, user, authLogout } = useContext(AuthContext)
-
-    useEffect(() => {
-        if (!user) {
-            navigate("/login");
-        }
-    }, [user]);
-
-  return (
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <div class="container">
-    <a class="navbar-brand" href="#">Admin</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarColor01">
-      <ul class="navbar-nav me-auto">
-        <li class="nav-item">
-          <a class="nav-link active" href="#">NavBar
-            <span class="visually-hidden">(current)</span>
-          </a>
-        </li>
-                  </ul>
-                  <ul class="navbar-nav ml-auto">
-                      <li class="nav-item">
-                          <a class="nav-link" href="#">
-                              <FaUser />
-                              {user ? user.username : ""}
-                          </a>
-                      </li>
-                      <li class="nav-item">
-                          <a class="nav-link" type="button"
-                              onClick={() => authLogout()}
-                          href="/"><FaSignOutAlt/></a>
-                      </li>
-                  
-                  </ul>
-                  
-                      
-      </div>
-    </div>
-</nav>
- )
+  )
  
 }
 
